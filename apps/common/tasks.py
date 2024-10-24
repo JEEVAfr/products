e3ab955e09ff2d56abcc92983d0a62895c637df1
JEEVAfr <jeevakumar1831@gmail.com>
2024-10-24 19:05:54 +0530

    message

--- apps/common/tasks.py
+++ apps/common/tasks.py
@@ -5,47 +5,64 @@ from celery import shared_task
 from apps.common.models.base import *
 from .serializers import *
 from rest_framework import status
+from .crud_views import *
+
 
 logger = logging.getLogger(__name__)
 
+
+def batched_query(queryset, batch_size=10): # retrive the data in batches
+
+    total = queryset.count()
+    for start in range(0, total, batch_size):
+        end = min(start + batch_size, total)
+        for entry in queryset[start:end]:
+            yield entry
+
 @shared_task
-def process_category(file_data):
+def process_category(file_data, batch_size=10):
     io_string = io.StringIO(file_data)
     reader = csv.reader(io_string, delimiter=',')
-    
-    errors = []
+
+    data_chunk = []
+
     for row in reader:
-        if not row:
+
+        if not row or len(row) < 4:
             continue
+
+        category_data = Category(
+            name=row[0],
+            description=row[1],
+            slug=row[2],
+            status=row[3].lower() == 'true',
+        )
         
-        if len(row) < 4:
-            errors.append(f'Row does not contain enough data: {row}')
-            continue
+        data_chunk.append(category_data)
 
-        category_data = {
-            'name': row[0],
-            'description': row[1],
-            'slug': row[2],
-            'status': row[3].lower() == 'true',
-        }
+        if len(data_chunk) >= batch_size:
+            save_categories(data_chunk)
+            data_chunk = []  
 
-        serializer = CategorySerializer(data=category_data)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            errors.append(serializer.errors)
+    if data_chunk:
+        save_categories(data_chunk)
 
-    if errors:
-        return {'status': 'failed', 'errors': errors}
-    
     return {'status': 'success', 'message': 'Categories uploaded successfully'}
 
+def save_categories(chunk):
+    try:
+        Category.objects.bulk_create(chunk)
+    except Exception as e:
+        print(f"Error during bulk create: {e}")
+
+
 @shared_task
-def process_subcategory(file_data):
+def process_subcategory(file_data, batch_size=10):
 
     io_string = io.StringIO(file_data)
     reader = csv.reader(io_string, delimiter=',')
 
+    data_chunk = []
     errors = []
     for row in reader:
 
@@ -70,43 +87,58 @@ def process_subcategory(file_data):
                 'status': row[4].lower() == 'true', 
             }
 
-        serializer = SubcategorySerializer(data = subcategory_data)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            errors.append(serializer.errors)
+        data_chunk.append(subcategory_data)
 
-        if errors:
-            return {'status': 'failed', 'errors': errors}
+        if len(data_chunk) >= batch_size:
+            save_subcategories(data_chunk)
+            data_chunk = []
 
+    
+    if data_chunk:
+        save_subcategories(data_chunk)
+
+    if errors:
+        return {'status': 'failed', 'errors': errors}
+    
     return {'status': 'success', 'message': 'Subcategories uploaded successfully'}
 
 
+def save_subcategories(chunk):
+
+    try:
+        Subcategory.objects.bulk_create(chunk)
+    except Exception as e:
+        print(f"Error during bulk create: {e}")
+
+
 @shared_task
-def process_product(file_data):
+def process_product(file_data, batch_size=10):
 
     io_string = io.StringIO(file_data)
     reader = csv.reader(io_string, delimiter=',')
 
     errors = []
+    data_chunk = []
     for row in reader:
 
         if not row:
             continue
             
         if len(row) < 9:
-            return errors.append({'error': f'Row does not contain enough data: {row}'}, status=status.HTTP_400_BAD_REQUEST)
+            errors.append({'error': f'Row does not contain enough data: {row}'}, status=status.HTTP_400_BAD_REQUEST)
+            continue
 
         try:
             category = Category.objects.get(id=row[0])
         except Category.DoesNotExist:
-            return errors.append({'error': f'Category with ID {row[0]} does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+            errors.append({'error': f'Category with ID {row[0]} does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+            continue
 
         try:
             subcategory = Subcategory.objects.get(id=row[1])
         except Subcategory.DoesNotExist:
-            return errors.append({'error': f'Subcategory with ID {row[1]} does not exist'}, status=status.HTTP_400_BAD_REQUEST)
-
+            errors.append({'error': f'Subcategory with ID {row[1]} does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+            continue
 
         product_data = {
                 'category': category.id, 
@@ -120,17 +152,24 @@ def process_product(file_data):
                 'rating': row[8],
         }
 
-        serializer = ProductSerializer(data=product_data)
+        data_chunk.append(product_data)
 
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            errors.append(serializer.errors)
+        if len(data_chunk) >= batch_size:
+            save_products(data_chunk)
+            data_chunk = []
 
-        if errors:
-            return {'status': 'failed', 'errors': errors}
+    if data_chunk:
+        save_products(data_chunk)
+
+    if errors:
+        return {'status': 'failed', 'errors': errors}
 
     return {'status': 'success', 'message': 'Subcategories uploaded successfully'}
 
 
+def save_products(chunk):
 
+    try:
+        Product.objects.bulk_create(chunk)
+    except Exception as e:
+        print(f"Error during bulk create: {e}")
